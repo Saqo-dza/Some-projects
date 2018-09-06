a37cac65d56518168e6eafb807b1eb5c071f4d30
Saqo-dza <42004684+Saqo-dza@users.noreply.github.com>
2018-09-06 22:16:32 +0400

    Update Program.cs

--- CoffeeMachine/CoffeeMachine/Program.cs
+++ CoffeeMachine/CoffeeMachine/Program.cs
@@ -22,7 +22,7 @@ namespace CoffeeMachine
             {
                 Console.Clear();
                 Console.WriteLine(e.Message);
-                Main();
+                coffeeStore.EnterCoins();
             }
             
         }
